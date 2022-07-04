85fc16591108a231d16f6cf260a5f70e06f9d539
valenfernandez <valenfernandez@users.noreply.github.com>
2022-07-04 11:38:27 -0300

    add: vistas de usuario

--- surgen/casos/urls.py
+++ surgen/casos/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="casos-home"),
+    path("perfil/", views.perfil, name="casos-perfil"),
+    path("perfil/caso/", views.caso, name="casos-caso"),
     path("acercade/", views.about, name="casos-about"),
 ]
--- surgen/casos/views.py
+++ surgen/casos/views.py
@@ -1,26 +1,45 @@
 from django.shortcuts import render
 
+victima = {
+    "nombre": "Alicia Perez",
+    "domicilio": "Av.Colón 1234",
+    "documento": "30 111 222",
+    "telefono": "+542235112233",
+    "email": "alicia_perez@gmail.com",
+    "fecha_nacimiento": "01/02/1985",
+    # contactos = ... # lista de Contactos
+}
+
 casos = [
     {
         "victima": "Alicia",
-        "agresor": "Bartolomé",
+        "agresor": "Bartolomé Diaz",
         "fecha": "12 Noviembre 2021",
     },
     {
-        "victima": "Carla",
-        "agresor": "Darío",
+        "victima": "Alicia",
+        "agresor": "Darío Martinez",
         "fecha": "12 de Noviembre 2021",
     },
-
 ]
 
-
 # Create your views here.
-def home(request):
+def perfil(request):
     context = {
+        "victima" : victima,
         "casos": casos,
     }
-    return render(request, "casos/home.html", context=context)
+    return render(request, "casos/perfil.html", context=context)
+
+def caso(request):
+    context = {
+        "caso": casos[0],
+    }
+    return render(request, "casos/caso.html", context=context)
+
+def home(request):
+    # TODO: Agregar redireccion al que esta logeado
+    return render(request, "casos/home.html")
 
 def about(request):
     return render(request, "casos/about.html", {"title": "Resultado del Hackaton Contra la Violencia de Género 2021"})
