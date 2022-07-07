5a11e3f72f04f72e1cc67e1877fe2835f81128b5
valenfernandez <valenfernandez@users.noreply.github.com>
2022-07-07 17:12:12 -0300

    add: vista de casos.

--- surgen/casos/views.py
+++ surgen/casos/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from .models import Caso
+from .models import Incidencia
+from .models import Documento
 
 # Create your views here.
 def perfil(request):
     context = {
-        "casos": Caso.objects.all(),
+        "victima": Caso.objects.first().victima,
+        "casos": Caso.objects.all(), # esto esta tirando error-> sera por db vacia?
     }
     return render(request, "casos/perfil.html", context=context)
 
 def caso(request):
     context = {
-        "caso": Caso.objects.first(),
+        "caso": Caso.objects.first(), # TODO aca seria solo el caso que pido el usuario
+        "historial" : Incidencia.objects.first(), #TODO aca se enviaria un arreglo ordenado de los eventos
+        "incidencias" : Incidencia.objects.first(), # TODO aca se deberian agregar las incidencias que correspondan al caso
+        "documentos" : Documento.objects.first() # TODO aca se deberian agregar los documentos que correspondan al caso
     }
     return render(request, "casos/caso.html", context=context)
 
