4c3f8306e479468834590c0903add47eff851f05
valenfernandez <valen.fernandez.montenegro@gmail.com>
2022-07-11 11:42:11 -0300

    add: Datos de contactos de victimas, pagina about y formación de historial

--- surgen/casos/views.py
+++ surgen/casos/views.py
@@ -2,30 +2,51 @@ from django.shortcuts import render
 from .models import Caso
 from .models import Incidencia
 from .models import Documento
+from .models import Contacto
 
 # Create your views here.
 def perfil(request):
     victima = Caso.objects.first().victima
     context = {
         "victima": victima,
+        "contactos": Contacto.objects.filter(victima = victima),
         "casos": Caso.objects.filter(victima = victima),
     }
     return render(request, "casos/perfil.html", context=context)
 
 def caso(request):
-    #El historial se compone de elementos con: nombre, descripcion y fecha 
     caso = Caso.objects.first() # TODO aca seria solo el caso que pido el usuario
+    incidencias = Incidencia.objects.filter(caso = caso)
+    documentos = Documento.objects.filter(caso = caso) 
+    historial = []
+    for inc in incidencias :
+        historial.add({
+            'nombre': inc.nombre,
+            'fecha': inc.fecha,
+            'descripcion': inc.descripcion
+            }
+        )
+    for doc in documentos :
+        historial.add({
+            'nombre': doc.nombre,
+            'fecha': doc.fecha,
+            'descripcion': doc.descripcion
+            }
+        )
     context = {
         "caso": caso, 
-        "historial" : Incidencia.objects.all(), #TODO aca se enviaria un arreglo ordenado de los eventos
-        "incidencias" : Incidencia.objects.filter(caso = caso),
-        "documentos" : Documento.objects.filter(caso = caso) 
+        "historial" : historial.orderby('-fecha'),
+        "incidencias" : incidencias,
+        "documentos" : documentos 
     }
     return render(request, "casos/caso.html", context=context)
 
 def home(request):
     # TODO: Agregar redireccion al que esta logeado
-    return render(request, "casos/home.html")
+    #if request.user.is_authenticated: 
+    #    return render(request, "casos/perfil.html")
+    #else: 
+        return render(request, "casos/home.html")
 
 def about(request):
-    return render(request, "casos/about.html", {"title": "Resultado del Hackaton Contra la Violencia de Género 2021"})
+    return render(request, "casos/about.html")
