68421c2fb24b9ef14ca586cce5c7befdb1da1c6b
valenfernandez <valen.fernandez.montenegro@gmail.com>
2022-09-19 11:08:12 -0300

    add: documentos pdf; excluir mimetype en admin

--- surgen/casos/admin.py
+++ surgen/casos/admin.py
@@ -2,6 +2,10 @@ from pydoc import Doc
 from django.contrib import admin
 from .models import Caso, Contacto, Domicilio, Victima, Agresor, Incidencia, Documento, Nota
 
+admin.site.site_header = 'Administrador Surgen'
+
+class DocumentoAdmin(admin.ModelAdmin):
+    exclude = ('mimetype', )
 
 # Register your models here.
 admin.site.register(Caso)
@@ -10,5 +14,5 @@ admin.site.register(Domicilio)
 admin.site.register(Victima)
 admin.site.register(Agresor)
 admin.site.register(Incidencia)
-admin.site.register(Documento)
+admin.site.register(Documento, DocumentoAdmin)
 admin.site.register(Nota)
