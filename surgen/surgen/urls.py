a678ad54eb1d687e6c7892b3cbb740e1076ca7f5
valenfernandez <valen.fernandez.montenegro@gmail.com>
2023-01-26 17:07:30 -0300

    add: Cambio de contraseña

--- surgen/surgen/urls.py
+++ surgen/surgen/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from usuarios import views as user_views
 from . import settings
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -23,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name = 'usuarios/login.html'), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'usuarios/logout.html'), name = 'logout'),
+    path("password_change/", user_views.password_change, name="password_change"),
     path('', include("casos.urls")),
 ]
 
